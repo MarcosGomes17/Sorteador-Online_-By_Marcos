892c156ae781da51c0a7f2629873cb22d65883c0
MarcosGomes17 <marcosvgomes07@outlook.com>
2025-05-28 10:13:54 -0300

    Alert update, using If and Else

--- scripts.js
+++ scripts.js
@@ -3,9 +3,15 @@ function generateNumber() {
   const min = Math.ceil(document.querySelector(".min").value);
   const max = Math.floor(document.querySelector(".max").value);
 
-  const result = Math.floor(Math.random() * (max - min + 1)) + min;
-
-  alert(result);
+  if (min >= max) {
+    alert('O valor minimo tem que ser MENOR que o valor máximo')
+  } 
   
+  else {
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    alert(result);
+  }
+
 }
 
